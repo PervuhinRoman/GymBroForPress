a2fc1ae3ca748ffa4eb2bebdc1d6a560b284c35c
Pervukhin Roman <rg.pervuhin5198@gmail.com>
2025-04-01 16:02:06 +0300

    Add MAPKIT_API_KEY for IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey(Bundle.main.object(forInfoDictionaryKey: "MAPKIT_API_KEY") as? String)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
