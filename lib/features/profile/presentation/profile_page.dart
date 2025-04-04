c9c20baf5ed28e9505369d988283793bb668b5d7
Gush <eugene0w0gush@gmail.com>
2025-04-04 11:34:22 +0000

    lasttt

--- lib/features/profile/presentation/profile_page.dart
+++ lib/features/profile/presentation/profile_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'info_body/settings_menu.dart';
 import 'profile_body.dart';
+import 'package:gymbro/features/auth/domain/auth_service.dart';
 
 class ProfilePage extends ConsumerWidget {
   const ProfilePage({
